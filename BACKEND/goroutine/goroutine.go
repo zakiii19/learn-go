df18df70fb258b7828f193fd1603bfae78472ead
zakiii19 <qolbulmuzaki@gmail.com>
2024-01-17 16:54:53 +0700

    goroutine

---
+++ BACKEND/goroutine/goroutine.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func print(till int, message string) {
+	for i := 0; i < till; i++ {
+		fmt.Println((i+1), message)
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	go print(5, "hallo")
+	go print(5, "apakabar")
+	var input string
+	fmt.Scanln(&input)
+}
